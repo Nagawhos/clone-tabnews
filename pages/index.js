1b04e8bbb2fa8eaa48b0bb83f2c9fcdaecd47123
walternagawo <111139548+Nagawhos@users.noreply.github.com>
2024-05-03 00:21:56 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <h1>
+      Meu amor, muito obrigado por todo o apoio e por tudo o que você tem feito por mim, eu
+      te amo muito e amo você pra sempre meu amor.
+    </h1>
+  );
+}
+
+export default Home;
